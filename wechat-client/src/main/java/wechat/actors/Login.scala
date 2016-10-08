37f91fbe9e7aa1dc2dcd821f7827131a10464309
redlion99 <binlish81@gmail.com>
2016-10-08 18:25:09 +0800

    handle message receive timeout

--- wechat-client/src/main/java/wechat/actors/Login.scala
+++ wechat-client/src/main/java/wechat/actors/Login.scala
@@ -1,6 +1,6 @@
 package wechat.actors
 
-import akka.actor.{Actor, ActorLogging, PoisonPill, Props}
+import akka.actor.{Actor, ActorLogging, PoisonPill, Props, ReceiveTimeout}
 import akka.actor.Actor.Receive
 import akka.http.scaladsl.model.{HttpHeader, HttpMethods, HttpRequest}
 import akka.http.scaladsl.model.Uri.Query
@@ -8,7 +8,7 @@ import akka.http.scaladsl.model.headers.RawHeader
 import org.joda.time.{DateTime, DateTimeZone}
 import spray.json.{JsString, _}
 
-import scala.concurrent.duration.FiniteDuration
+import scala.concurrent.duration.{Duration, FiniteDuration}
 import scala.util.{Failure, Success}
 import scala.xml._
 import scala.collection.immutable
@@ -17,223 +17,240 @@ import scala.collection.immutable
   * Created by libin on 16/9/28.
   */
 
-case class QrCode(uuid:String){
-  override def toString: String = "https://login.weixin.qq.com/qrcode/"+uuid
+case class QrCode(uuid: String) {
+  override def toString: String = "https://login.weixin.qq.com/qrcode/" + uuid
 }
 
-case class CheckLogin(uuid:String,tip:String="0",tries:Int=0)
+case class CheckLogin(uuid: String, tip: String = "0", tries: Int = 0)
 
-class Login(val httpPool: HttpPool, props:Props) extends HttpClient with ActorLogging{
+class Login(val httpPool: HttpPool, props: Props) extends HttpClient with ActorLogging {
   implicit val dispatcher = httpPool.httpDispatcher
+
+  context.setReceiveTimeout(Duration(1,"minute"))
+
   override def receive: Receive = {
-    case ("start",sessionId:String) =>
+    case ("start", sessionId: String) =>
       val me = self
       val oSender = sender()
       httpPool.sendRequest(
         GetParams("https://login.weixin.qq.com/jslogin"
-          ,Map("appid"->"wx782c26e4c19acffb","fun"->"new","lang"->"zh_CN","_"->DateTime.now().getMillis.toString))
-        ,"login").map(parseTextResponse(_)).filter(_.contains(("window.QRLogin.code","200"))).map(_.get("window.QRLogin.uuid")).onComplete{
-        case Success(Some(uuid))=>
-          me ! QrCode(uuid.slice(1,uuid.length-1))
-          oSender ! QrCode(uuid.slice(1,uuid.length-1))
+          , Map("appid" -> "wx782c26e4c19acffb", "fun" -> "new", "lang" -> "zh_CN", "_" -> DateTime.now().getMillis.toString))
+        , "login").map(parseTextResponse(_)).filter(_.contains(("window.QRLogin.code", "200"))).map(_.get("window.QRLogin.uuid")).onComplete {
+        case Success(Some(uuid)) =>
+          me ! QrCode(uuid.slice(1, uuid.length - 1))
+          oSender ! QrCode(uuid.slice(1, uuid.length - 1))
         case Failure(e) =>
-          log.error(e,"get qrcode failed")
+          log.error(e, "get qrcode failed")
       }
-    case qr@QrCode(uuid:String) =>
+    case qr@QrCode(uuid: String) =>
       log.info(qr.toString)
-      context.system.scheduler.scheduleOnce(FiniteDuration(10,"seconds"),self,CheckLogin(uuid))
-    case CheckLogin(uuid:String,tip,tries)=>
+      context.system.scheduler.scheduleOnce(FiniteDuration(10, "seconds"), self, CheckLogin(uuid))
+    case CheckLogin(uuid: String, tip, tries) =>
       httpPool.sendRequest(GetParams("https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login"
-        ,Map("tip"->tip,"uuid"->uuid,"_"->DateTime.now().getMillis.toString)),"check")
-        .map(parseTextResponse(_)).onComplete{
-        case Success(q:Query) if q.contains("window.code"->"408")=>
+        , Map("tip" -> tip, "uuid" -> uuid, "_" -> DateTime.now().getMillis.toString)), "check")
+        .map(parseTextResponse(_)).onComplete {
+        case Success(q: Query) if q.contains("window.code" -> "408") =>
           log.info(s" wait for auth $tip $tries ")
-          if(tries<10)
-            context.system.scheduler.scheduleOnce(FiniteDuration(1,"seconds"),self,CheckLogin(uuid,tip,tries+1))
+          if (tries < 10)
+            context.system.scheduler.scheduleOnce(FiniteDuration(1, "seconds"), self, CheckLogin(uuid, tip, tries + 1))
           else
             log.warning("login timeout session stoped")
-        case Success(q:Query) if q.contains("window.code"->"201")=>
-          context.system.scheduler.scheduleOnce(FiniteDuration(1,"seconds"),self,CheckLogin(uuid,tip,tries+1))
-        case Success(q:Query) if q.contains("window.code"->"200")=>
-          q.get("window.redirect_uri").foreach{
-            url=>
-              context.actorOf(props,sessionId) ! ("start", sessionId, url.slice(1,url.length-1))
+        case Success(q: Query) if q.contains("window.code" -> "201") =>
+          context.system.scheduler.scheduleOnce(FiniteDuration(1, "seconds"), self, CheckLogin(uuid, tip, tries + 1))
+        case Success(q: Query) if q.contains("window.code" -> "200") =>
+          q.get("window.redirect_uri").foreach {
+            url =>
+              context.actorOf(props, sessionId) !("start", sessionId, url.slice(1, url.length - 1))
           }
-        case Failure(e)=>
-          log.error(e,"login check")
-          if(tries<10)
-            context.system.scheduler.scheduleOnce(FiniteDuration(1,"seconds"),self,CheckLogin(uuid,tip,tries+1))
+        case Failure(e) =>
+          log.error(e, "login check")
+          if (tries < 10)
+            context.system.scheduler.scheduleOnce(FiniteDuration(1, "seconds"), self, CheckLogin(uuid, tip, tries + 1))
           else
             log.warning("login timeout")
       }
+    case ReceiveTimeout =>
+      self ! PoisonPill
     case _ =>
   }
 
-  val sessionId: String ={
+  val sessionId: String = {
     self.path.name
   }
 }
 
 
-case class Contact(userName:String, nickName:String, contactFlag:Int)
+case class Contact(userName: String, nickName: String, contactFlag: Int)
 
 
-case class SessionState(pass_ticket:String="", skey:String="", uin:String="", sid:String="")
-case class ClientState(user:JsObject,clientVersion:String)
+case class SessionState(pass_ticket: String = "", skey: String = "", uin: String = "", sid: String = "")
 
-case class SyncKey(key: JsObject){
+case class ClientState(user: JsObject, clientVersion: String)
+
+case class SyncKey(key: JsObject) {
   override def toString: String = {
     (key.getFields("List") match {
-      case Seq(m:JsArray)=>
-        m.elements.map(_.asJsObject.getFields("Key","Val") match {
-          case Seq(JsNumber(k),JsNumber(v))=>
-            k.toInt+"_"+v.toInt
+      case Seq(m: JsArray) =>
+        m.elements.map(_.asJsObject.getFields("Key", "Val") match {
+          case Seq(JsNumber(k), JsNumber(v)) =>
+            k.toInt + "_" + v.toInt
         })
     }).mkString("|")
   }
 }
 
-case class SyncCheck(syncKey:SyncKey)
+case class SyncCheck(syncKey: SyncKey)
 
-case class Sync(syncKey:SyncKey)
+case class Sync(syncKey: SyncKey)
 
-class InitActor(val httpPool: HttpPool) extends HttpClient with ActorLogging{
+class InitActor(val httpPool: HttpPool) extends HttpClient with ActorLogging {
   implicit val dispatcher = httpPool.httpDispatcher
+  context.setReceiveTimeout(Duration(1,"minute"))
   var state = SessionState()
-  var clientState:Option[ClientState] = None
+  var clientState: Option[ClientState] = None
 
-  var cookieHeaders:immutable.Seq[RawHeader] = immutable.Seq.empty
+  var cookieHeaders: immutable.Seq[RawHeader] = immutable.Seq.empty
 
-  var contactListSaved:Map[String,Contact] = Map.empty
+  var contactListSaved: Map[String, Contact] = Map.empty
 
-  def baseRequest(): JsObject ={
-    JsObject("Uin"->JsString(state.uin), "Sid"->JsString(state.sid)
-      , "SKey"->JsString(state.skey), "DeviceID"->JsString(sessionId))
+  var loginUrl = ""
+
+  def baseRequest(): JsObject = {
+    JsObject("Uin" -> JsString(state.uin), "Sid" -> JsString(state.sid)
+      , "SKey" -> JsString(state.skey), "DeviceID" -> JsString(sessionId))
   }
 
   override def receive: Receive = {
-    case ("start", sessionId:String, url:String) =>
+    case ("start", sessionId: String, url: String) =>
       val me = self
-      httpPool.sendRequestRequireCookies(HttpRequest(HttpMethods.GET,url),"login confirm").onComplete{
-        case Success((response,cookieOption))=>
+      loginUrl = url
+      httpPool.sendRequestRequireCookies(HttpRequest(HttpMethods.GET, url), "login confirm").onComplete {
+        case Success((response, cookieOption)) =>
           val xml = XML.loadString(response)
           log.info(xml.toString())
-          (xml\\"ret").text match {
+          (xml \\ "ret").text match {
             case "0" =>
-              val skey = (xml\\"skey").text
-              val wxsid = (xml\\"wxsid").text
-              val wxuin = (xml\\"wxuin").text
-              val pass_ticket = (xml\\"pass_ticket").text
-              me ! cookieOption.map(new RawHeader("Cookie",_) )
+              val skey = (xml \\ "skey").text
+              val wxsid = (xml \\ "wxsid").text
+              val wxuin = (xml \\ "wxuin").text
+              val pass_ticket = (xml \\ "pass_ticket").text
+              me ! cookieOption.map(new RawHeader("Cookie", _))
 
               me ! SessionState(pass_ticket, skey, wxuin, wxsid)
-
+            case _ =>
+              me ! PoisonPill
 
           }
-        case Failure(e)=>
-          log.error(e,"login confirm")
+        case Failure(e) =>
+          log.error(e, "login confirm")
 
       }
-    case cookies:immutable.Seq[RawHeader] =>
-      cookieHeaders=cookies
-    case s@SessionState(pass_ticket, skey, uid, wxsid)=>
+    case cookies: immutable.Seq[RawHeader] =>
+      cookieHeaders = cookies
+    case s@SessionState(pass_ticket, skey, uid, wxsid) =>
       log.info("state: " + s)
       state = s
       val me = self
-      val json = JsObject("BaseRequest"->baseRequest())
+      val json = JsObject("BaseRequest" -> baseRequest())
       httpPool.sendRequest(
         PostJson(s"https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxinit?pass_ticket=${state.pass_ticket}&lang=zh_CN&r=${~DateTime.now().getMillis}"
-          ,json).withHeaders(cookieHeaders) , "init").onComplete{
+          , json).withHeaders(cookieHeaders), "init").onComplete {
         case Success(response) =>
           val jsObject = response.parseJson.asJsObject
-          jsObject.getFields("User","ContactList","ClientVersion","SyncKey") match {
-            case Seq(user:JsObject,contactList:JsArray, JsNumber(version), key:JsObject)=>
-              me ! ClientState(user,version.toString())
+          jsObject.getFields("User", "ContactList", "ClientVersion", "SyncKey") match {
+            case Seq(user: JsObject, contactList: JsArray, JsNumber(version), key: JsObject) =>
+              me ! ClientState(user, version.toString())
               me ! SyncCheck(SyncKey(key))
-              me ! ("ContactList",contactList)
+              me !("ContactList", contactList)
             case x =>
               log.warning(s"unknown $x")
           }
-        case Failure(e)=>
-          log.error(e,"session init")
+        case Failure(e) =>
+          log.error(e, "session init")
       }
 
-    case s:ClientState=>
-      log.info("client info: "+ s)
-      clientState=Some(s)
-    case ("ContactList",contactList:JsArray)=>
-      contactListSaved = contactList.elements.map(_.asJsObject.getFields("UserName", "NickName","ContactFlag") match {
-      case Seq(JsString(userName),JsString(nickName), JsNumber(contactFlag))=>
-        (userName,Contact(userName, nickName, contactFlag.toInt ))
-    }).toMap
-    case SyncCheck(syncKey:SyncKey)=>
+    case s: ClientState =>
+      log.info("client info: " + s)
+      clientState = Some(s)
+    case ("ContactList", contactList: JsArray) =>
+      contactListSaved = (contactList.elements.map(_.asJsObject.getFields("UserName", "NickName", "ContactFlag") match {
+        case Seq(JsString(userName), JsString(nickName), JsNumber(contactFlag)) =>
+          (userName, Contact(userName, nickName, contactFlag.toInt))
+      }) ++ clientState.map(_.user).map(_.getFields("UserName", "NickName") match {
+        case Seq(JsString(userName), JsString(nickName)) =>
+          (userName, Contact(userName, nickName, 0))
+      })).toMap
+    case SyncCheck(syncKey: SyncKey) =>
       val me = self
       httpPool.sendRequest(GetParams("https://webpush.weixin.qq.com/cgi-bin/mmwebwx-bin/synccheck"
-        ,Map("r"->DateTime.now().getMillis.toString, "skey"->state.skey, "uin"->state.uin, "sid"->state.sid, "deviceid"->sessionId, "synckey"->syncKey.toString,"_"->DateTime.now().getMillis.toString))
-        .withHeaders(cookieHeaders),"SyncCheck")
-        .map(parseTextResponse(_)).onComplete{
-          case Success(q:Query) =>
-            log.info(q.get("window.synccheck").toString)
-            q.get("window.synccheck").map(x=>(x.slice(1,x.length-1)).split(","))
-              .foreach{
-                case Array("retcode:\"1101\"",_)=>
-                  me ! PoisonPill
-                case Array(_,"selector:\"0\"")=>
-                  log.info(s"nothing to get ")
-                  me ! SyncCheck(syncKey)
-                case s =>
-                  me ! Sync(syncKey)
-              }
+        , Map("r" -> DateTime.now().getMillis.toString, "skey" -> state.skey, "uin" -> state.uin, "sid" -> state.sid, "deviceid" -> sessionId, "synckey" -> syncKey.toString, "_" -> DateTime.now().getMillis.toString))
+        .withHeaders(cookieHeaders), "SyncCheck")
+        .map(parseTextResponse(_)).onComplete {
+        case Success(q: Query) =>
+          log.info(q.get("window.synccheck").toString)
+          q.get("window.synccheck").map(x => (x.slice(1, x.length - 1)).split(","))
+            .foreach {
+              case Array("retcode:\"1101\"", _) =>
+                log.warning(s"synccheck failed $q")
+                me ! ("start", sessionId, loginUrl)
+              case Array(_, "selector:\"0\"") =>
+                log.info(s"nothing to get ")
+                me ! SyncCheck(syncKey)
+              case s =>
+                me ! Sync(syncKey)
+            }
 
+        case Failure(e) =>
+          log.info(e.getMessage)
+          me ! SyncCheck(syncKey)
+      }
+    case Sync(syncKey: SyncKey) =>
+      val me = self
+      val json = JsObject("BaseRequest" -> baseRequest(), "SyncKey" -> syncKey.key, "rr" -> JsNumber(~DateTime.now().getMillis))
+      //log.info(json.prettyPrint)
+      httpPool.sendRequest(PostJson(s"https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=${state.sid}&skey=${state.skey}&pass_ticket=${state.pass_ticket}&lang=zh_CN&r=${DateTime.now().getMillis.toString}"
+        , json).withHeaders(cookieHeaders), "SyncMsg")
+        .onComplete {
+          case Success(response: String) =>
+            response.parseJson.asJsObject.getFields("BaseResponse", "AddMsgList", "SyncKey") match {
+              case x@Seq(base: JsObject, msgList: JsArray, key: JsObject) =>
+                log.info(x.toString())
+                base.getFields("Ret") match {
+                  case Seq(JsNumber(ret)) if ret == 0 =>
+                    msgList.elements.map(_.asJsObject.getFields("Content", "FromUserName", "ToUserName")).foreach {
+                      case Seq(c@JsString(content), f@JsString(from), t@JsString(to)) =>
+                        val msg = (contactListSaved.get(from), contactListSaved.get(to)) match {
+                          case (Some(contactFrom), Some(contactTo)) =>
+                            JsObject("userName" -> f, "content" -> c, "timestamp" -> JsString(DateTime.now().withZone(DateTimeZone.UTC).toString)
+                              , "contactFrom" -> JsString(contactFrom.nickName), "contactTo" -> JsString(contactTo.nickName))
+                          case (Some(contactFrom), _) =>
+                            JsObject("userName" -> f, "content" -> c, "timestamp" -> JsString(DateTime.now().withZone(DateTimeZone.UTC).toString)
+                              , "contactFrom" -> JsString(contactFrom.nickName))
+                          case (_, Some(contactTo)) =>
+                            JsObject("userName" -> f, "content" -> c, "timestamp" -> JsString(DateTime.now().withZone(DateTimeZone.UTC).toString)
+                              , "contactTo" -> JsString(contactTo.nickName))
+                          case _ =>
+                            JsObject("userName" -> f, "content" -> c, "timestamp" -> JsString(DateTime.now().withZone(DateTimeZone.UTC).toString))
+                        }
+                        log.info(msg.prettyPrint)
+
+                    }
+                    me ! SyncCheck(SyncKey(key))
+                  case _ =>
+                }
+
+
+            }
           case Failure(e) =>
             log.info(e.getMessage)
             me ! SyncCheck(syncKey)
         }
-    case Sync(syncKey:SyncKey)=>
-      val me = self
-      val json = JsObject("BaseRequest"->baseRequest(), "SyncKey"->syncKey.key, "rr"->JsNumber(~DateTime.now().getMillis))
-      //log.info(json.prettyPrint)
-      httpPool.sendRequest(PostJson(s"https://wx.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=${state.sid}&skey=${state.skey}&pass_ticket=${state.pass_ticket}&lang=zh_CN&r=${DateTime.now().getMillis.toString}"
-        ,json).withHeaders(cookieHeaders),"SyncMsg")
-        .onComplete{
-        case Success(response:String)=>
-          response.parseJson.asJsObject.getFields("BaseResponse","AddMsgList","SyncKey") match {
-            case x@Seq(base:JsObject,msgList:JsArray,key:JsObject) =>
-              log.info(x.toString())
-              base.getFields("Ret") match {
-                case Seq(JsNumber(ret)) if ret==0 =>
-                  msgList.elements.map(_.asJsObject.getFields("Content","FromUserName", "ToUserName")).foreach{
-                    case Seq(c@JsString(content), f@JsString(from), t@JsString(to))=>
-                      val msg = (contactListSaved.get(from), contactListSaved.get(to)) match {
-                        case (Some(contactFrom),Some(contactTo)) =>
-                          JsObject("userName"->f, "content"->c, "timestamp"->JsString(DateTime.now().withZone(DateTimeZone.UTC).toString)
-                            , "contactFrom"-> JsString(contactFrom.nickName), "contactTo"-> JsString(contactTo.nickName))
-                        case (Some(contactFrom),_) =>
-                          JsObject("userName"->f, "content"->c, "timestamp"->JsString(DateTime.now().withZone(DateTimeZone.UTC).toString)
-                            , "contactFrom"-> JsString(contactFrom.nickName))
-                        case (_,Some(contactTo)) =>
-                          JsObject("userName"->f, "content"->c, "timestamp"->JsString(DateTime.now().withZone(DateTimeZone.UTC).toString)
-                            ,  "contactTo"-> JsString(contactTo.nickName))
-                        case _ =>
-                          JsObject("userName"->f, "content"->c, "timestamp"->JsString(DateTime.now().withZone(DateTimeZone.UTC).toString))
-                      }
-                      log.info(msg.prettyPrint)
-
-                  }
-                  me ! SyncCheck(SyncKey(key))
-                case _ =>
-              }
-
-
-          }
-        case Failure(e)=>
-          log.info(e.getMessage)
-          me ! SyncCheck(syncKey)
-      }
+    case ReceiveTimeout =>
+      self ! PoisonPill
     case _ =>
   }
 
-  val sessionId: String ={
+  val sessionId: String = {
     self.path.name
   }
 }
